c5dce14e8e8001d51ff2ce9d4433ed8f26a15468
Kingpingtwo <108818695+Kingpingtwo@users.noreply.github.com>
2023-02-17 17:31:20 +0200

    Add files via upload

---
+++ IT/branching.js
@@ -0,0 +1,19 @@
+// Розгалуження.
+// if
+
+// if (5 < 10) {
+//   console.log(x < y);
+// } else {
+//   console.log(x > y);
+// }
+
+const salary = 1000;
+
+if (salary > 500) {
+  console.log("1");
+} else if (salary < 500) {
+  console.log("2");
+}
+
+// Розгалуження.
+// if else
---
+++ IT/ternary-operator.js
@@ -0,0 +1,16 @@
+// - Тернарний оператор.
+
+let balance = 1000;
+let message;
+
+if (balance >= 0) {
+  message = "Позитивний баланс";
+} else {
+  message = "Негативний баланс";
+}
+
+// message = balance >= 0 ? "Позитивний баланс" : "Негативний баланс";
+
+// console.log(message);
+
+console.log(message);
